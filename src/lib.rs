1a23cd4533f3f7bbf61a17f5a4ab84c12f440928
Andrei Zolkin <58516282+Z4RX@users.noreply.github.com>
2021-09-09 12:07:05 +0300

    add graphql, add service, refactoring handlers (#7)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #[macro_use]
+extern crate async_graphql;
+#[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate serde;
 
+use async_graphql::{EmptySubscription, Schema};
 use axum::{
     body::Body,
     handler::{get, post},
@@ -14,16 +17,25 @@ use sqlx::PgPool;
 use tower::ServiceBuilder;
 use tower_http::{set_header::SetResponseHeaderLayer, trace::TraceLayer};
 
+mod dto;
 mod error;
 mod extractors;
+mod graphql;
 mod handlers;
 mod model;
+mod service;
 mod sql;
 mod utils;
 
 pub mod config;
 
 pub fn app(pg_pool: PgPool) -> Router<BoxRoute> {
+    use self::graphql::{AppSchema, MutationRoot, QueryRoot};
+
+    let schema: AppSchema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
+        .data(pg_pool.to_owned())
+        .finish();
+
     let middleware_stack = ServiceBuilder::new()
         .layer(TraceLayer::new_for_http())
         .layer(SetResponseHeaderLayer::<_, Body>::overriding(
@@ -42,6 +54,7 @@ pub fn app(pg_pool: PgPool) -> Router<BoxRoute> {
             header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
             HeaderValue::from_static("true"),
         ))
+        .layer(AddExtensionLayer::new(schema))
         .layer(AddExtensionLayer::new(pg_pool))
         .into_inner();
 
@@ -49,6 +62,10 @@ pub fn app(pg_pool: PgPool) -> Router<BoxRoute> {
         .route("/login", post(handlers::login))
         .route("/register", post(handlers::register))
         .route("/authorize", get(handlers::authorize))
+        .route(
+            "/graphql",
+            get(handlers::graphql_playground).post(handlers::graphql),
+        )
         .layer(middleware_stack)
         .boxed()
 }
--- src/model.rs
+++ src/model.rs
@@ -2,13 +2,15 @@ use chrono::{DateTime, Utc};
 use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
+#[derive(Debug, Serialize, Deserialize, Clone, FromRow, SimpleObject)]
 pub struct User {
     #[serde(skip_serializing)]
+    #[graphql(skip)]
     pub id: Uuid,
     pub name: String,
     pub email: String,
     #[serde(skip_serializing)]
+    #[graphql(skip)]
     pub password: String,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
